d65fd66ca5a821fdce30b2f4fa17a3a10022261a
harmisteravadiya <124142407+harmisteravadiya@users.noreply.github.com>
2024-06-02 11:15:28 +0530

    Intial

--- index.js
+++ index.js
@@ -3,7 +3,11 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+import App from './src/App';
 import {name as appName} from './app.json';
+import TrackPlayer from 'react-native-track-player';
+import { MusicService } from './MusicService';
 
+
+TrackPlayer.registerPlaybackService(()=>MusicService)
 AppRegistry.registerComponent(appName, () => App);
---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import type {PropsWithChildren} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+} from 'react-native';
+
+
+function App(): React.JSX.Element {
+  return (
+    <SafeAreaView>
+      <StatusBar />
+      
+    </SafeAreaView>
+  );
+}
+
+export default App;
