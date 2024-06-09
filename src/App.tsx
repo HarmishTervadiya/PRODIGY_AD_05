736ddd77f561563c01e56ede1d5bcd142258327e
harmistervadiya <154576954+HarmishTervadiya@users.noreply.github.com>
2024-06-09 10:03:54 +0530

    Completed

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,83 @@ import React,{useState, useEffect, useRef} from 'react';
 import type {PropsWithChildren} from 'react';
 import {
   SafeAreaView,
-  ScrollView,
   StatusBar,
-  TextInput,
   View,
   StyleSheet,
   Text,
-  TouchableOpacity,
-  Animated,
-  Easing,
-  useAnimatedValue,
-  FlatList
+  Button,
 } from 'react-native';
 
+interface TimerProps{
+  minutes:number,
+  seconds:number,
+  miliseconds:number,
+}
+
 function App(): React.JSX.Element {
+  const [time, setTime] = useState({ minutes: 0, seconds: 0, milliseconds: 0 });
+  const [isRunning, setIsRunning] = useState(false);
+  const timerRef = useRef(null);
+  const [progress, setProgress] = useState(0);
+
+  const startStopwatch = () => {
+    if (!isRunning) {
+      setIsRunning(true);
+      timerRef.current = setInterval(() => {
+        setTime((prevTime) => {
+          let { minutes, seconds, milliseconds } = prevTime;
+
+          milliseconds += 1;
+
+          if (milliseconds >= 100) {
+            milliseconds = 0;
+            seconds += 1;
+          }
+
+          if (seconds >= 60) {
+            seconds = 0;
+            minutes += 1;
+          }
+
+          setProgress((minutes * 60 + seconds) % 60 / 60);
+
+
+          return { minutes, seconds, milliseconds };
+        });
+      }, 10);
+    }
+  };
+
+  const stopStopwatch = () => {
+    if (isRunning) {
+      clearInterval(timerRef.current);
+      setIsRunning(false);
+    }
+  };
+
+  const resetStopwatch = () => {
+    clearInterval(timerRef.current);
+    setIsRunning(false);
+    setTime({ minutes: 0, seconds: 0, milliseconds: 0 });
+    setProgress(0);
+  };
 
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar />
-    
-    
+      <View style={styles.container}>
+        <View style={[styles.wrapper]}>
+        <Text style={styles.timeText}>
+          {String(time.minutes).padStart(2, '0')}:
+          {String(time.seconds).padStart(2, '0')}:
+          {String(time.milliseconds).padStart(2, '0')}
+        </Text>
+      </View>
+      <View style={styles.buttonContainer}>
+        <Button title={isRunning ? 'Stop' : 'Start'} onPress={isRunning ? stopStopwatch : startStopwatch} />
+        <Button title="Reset" onPress={resetStopwatch} />
+      </View>
+    </View>
     </SafeAreaView>
   );
 }
@@ -29,13 +86,35 @@ function App(): React.JSX.Element {
 
 const styles=StyleSheet.create({
   container: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    paddingTop:30,
+    alignItems: 'center',
+  },
+  wrapper:{
+    borderWidth:2,
+    borderRadius:200,
+    borderColor:'#4190ff',
+    padding:40,
+    height:260,
+    justifyContent:'center',
+    marginBottom:30
+  },
+    svg: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+  },
+  timeText: {
+    fontSize: 48,
+  },
+  buttonContainer: {
     flex:1,
-    backgroundColor:'#f1f1f1d6',
-    maxHeight:'100%',
-    justifyContent:'flex-end',
-    paddingVertical:20,
-    // paddingHorizontal:10,
-    height:'100%'
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '45%',
+    alignItems:'flex-start',
+    
   },
 })
 
